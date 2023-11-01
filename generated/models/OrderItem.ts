c8209f114f2f9f62251831b87de27a329c8fd9ca
aaaaa-pi <770358634@qq.com>
2023-11-01 19:52:13 +0800

    update .gitignore

--- generated/models/OrderItem.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type OrderItem = {
-    asc?: boolean;
-    column?: string;
-};
--- generated/models/UserLoginRequest.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type UserLoginRequest = {
-    userAccount?: string;
-    userPassword?: string;
-};
